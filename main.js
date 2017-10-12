d6a90f06de52dd51eecdd98b3b805e0daaaed899
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-10-12 10:08:42 +0200

    1/ es6 + switch statement implementation

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-function counter(state = 0, action) {
-  if (typeof state === 'undefined') {
-    return 0;
-  }
-
-  if (action.type === 'INCREMENT') {
-    return state + 1;
-  } else if (action.type === 'DECREMENT') {
-    return state - 1;
-  } else {
-    return state;
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
   }
 }
 
